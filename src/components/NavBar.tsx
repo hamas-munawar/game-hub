dd832dfd23416eb4da6f16e6f5be270127d58cf2
Hamas Munawar <muhammadhamasmunawar@hotmail.com>
2025-07-07 12:54:38 +0500

    built a colormode switch on navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,25 @@
-import { HStack, Image, Button } from "@chakra-ui/react";
-import logo from "../assets/logo.webp";
+import { HStack, Image, Switch } from "@chakra-ui/react";
 import { useColorMode } from "./ui/color-mode";
+import { Icon } from "@chakra-ui/react";
+import { FaMoon, FaSun } from "react-icons/fa";
+import logo from "../assets/logo.webp";
 
 const NavBar = () => {
   const { toggleColorMode } = useColorMode();
 
   return (
-    <HStack>
+    <HStack justifyContent="space-between" paddingInline={4}>
       <Image src={logo} boxSize="60px" />
       Navbar
-      <Button onClick={toggleColorMode}>Toggle Mode</Button>
+      <Switch.Root colorPalette="blue" size="lg" onChange={toggleColorMode}>
+        <Switch.HiddenInput />
+        <Switch.Control>
+          <Switch.Thumb />
+          <Switch.Indicator fallback={<Icon as={FaMoon} color="gray.400" />}>
+            <Icon as={FaSun} color="yellow.400" />
+          </Switch.Indicator>
+        </Switch.Control>
+      </Switch.Root>
     </HStack>
   );
 };
