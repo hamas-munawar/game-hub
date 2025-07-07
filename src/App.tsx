27ac9a7753d42ee2405cead0e861e9a851d838b8
Hamas Munawar <muhammadhamasmunawar@hotmail.com>
2025-07-07 12:18:20 +0500

    build basic navbar using chakra ui

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { Grid, GridItem } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
+
+const App = () => {
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav">
+        <NavBar />
+      </GridItem>
+      <GridItem area="aside" bg="gold" display={{ base: "none", lg: "block" }}>
+        Aside
+      </GridItem>
+      <GridItem area="main" bg="dodgerblue">
+        Main Content
+      </GridItem>
+    </Grid>
+  );
+};
+
+export default App;
