6785d8a6e6e73ae953fe939fa41b403c7696719a
Hamas Munawar <muhammadhamasmunawar@hotmail.com>
2025-07-10 10:39:51 +0500

    cleaning up genres added heading on genre list

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,4 +1,4 @@
-import { HStack, Image, Link, List, Text } from "@chakra-ui/react";
+import { Heading, HStack, Image, Link, List, Text } from "@chakra-ui/react";
 
 import useGenres from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
@@ -16,31 +16,37 @@ const GenreList = ({ genre, onSelectGenre }: Props) => {
   if (error) return null;
 
   return (
-    <List.Root listStyle="none" gap={4}>
-      {isLoading &&
-        Array.from({ length: 18 }).map((_, index) => (
-          <GenreSkeleton key={index} />
+    <>
+      <Heading as="h2" size="lg" marginBlock={4}>
+        Genres
+      </Heading>
+      <List.Root listStyle="none" gap={4}>
+        {isLoading &&
+          Array.from({ length: 18 }).map((_, index) => (
+            <GenreSkeleton key={index} />
+          ))}
+        {genres.map((g) => (
+          <List.Item key={g.id} _hover={{ cursor: "pointer" }}>
+            <Link onClick={() => onSelectGenre(g)}>
+              <HStack>
+                <Image
+                  src={getCroppedImageUrl(g.image_background)}
+                  boxSize="32px"
+                  objectFit={"cover"}
+                  borderRadius={5}
+                />
+                <Text
+                  fontSize="lg"
+                  textDecoration={g.id == genre?.id ? "underline" : "none"}
+                >
+                  {g.name}
+                </Text>
+              </HStack>
+            </Link>
+          </List.Item>
         ))}
-      {genres.map((g) => (
-        <List.Item key={g.id} _hover={{ cursor: "pointer" }}>
-          <Link onClick={() => onSelectGenre(g)}>
-            <HStack>
-              <Image
-                src={getCroppedImageUrl(g.image_background)}
-                boxSize="32px"
-                borderRadius={5}
-              />
-              <Text
-                fontSize="lg"
-                textDecoration={g.id == genre?.id ? "underline" : "none"}
-              >
-                {g.name}
-              </Text>
-            </HStack>
-          </Link>
-        </List.Item>
-      ))}
-    </List.Root>
+      </List.Root>
+    </>
   );
 };
 
