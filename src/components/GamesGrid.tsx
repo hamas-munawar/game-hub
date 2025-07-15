8452825e304cd1204ea3443263994b7c656a40d7
Hamas Munawar <muhammadhamasmunawar@hotmail.com>
2025-07-15 11:32:19 +0500

    Refactoring: Removed gameCardContainer as it is no longer require previouslly we were using it to keep consistent layout of our games and skelton of games.

--- src/components/GamesGrid.tsx
+++ src/components/GamesGrid.tsx
@@ -2,7 +2,6 @@ import { Button, SimpleGrid, Text } from '@chakra-ui/react';
 
 import useGames from '../hooks/useGames';
 import GameCard from './common/GameCard';
-import GameCardContainer from './common/GameCardContainer';
 import GameCardSkeleton from './common/GameCardSkeleton';
 
 import type { GameQuery } from "../App";
@@ -36,16 +35,10 @@ const GamesGrid = ({ gameQuery }: Props) => {
       >
         {isLoading &&
           Array.from({ length: 12 }).map((_, index) => (
-            <GameCardContainer key={index}>
-              <GameCardSkeleton />
-            </GameCardContainer>
+            <GameCardSkeleton key={index} />
           ))}
         {data?.pages.map((page) =>
-          page.results.map((game) => (
-            <GameCardContainer key={game.id}>
-              <GameCard game={game} />
-            </GameCardContainer>
-          ))
+          page.results.map((game) => <GameCard game={game} key={game.id} />)
         )}
       </SimpleGrid>
       {hasNextPage && (
