53fa1e79928548253ab351ec1a8d9d938d76e61c
Hamas Munawar <muhammadhamasmunawar@hotmail.com>
2025-07-15 06:49:02 +0500

    installed and setup react-query

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,23 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { Provider } from "./components/ui/provider.tsx";
-import "./index.css";
-import App from "./App.tsx";
+import './index.css';
+
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+import App from './App.tsx';
+import { Provider } from './components/ui/provider.tsx';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </Provider>
   </StrictMode>
 );
